a568b694835bc9298166d531cab109ede80666bb
Daniel77apr <105032333+Daniel77apr@users.noreply.github.com>
2024-04-29 22:44:09 -0600

    Add animations

---
+++ JavaScript/script.js
@@ -0,0 +1,18 @@
+let sections = document.querySelectorAll("section");
+
+sections[0].classList.add("show-animate");
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -500;
+        let height = sec.offsetHeight;
+
+        if (top >= offset && top < offset + height) {
+            sec.classList.add( "show-animate")
+        } /*else {
+            sec.classList.remove("show-animate")
+        }*/
+    
+    })
+}
