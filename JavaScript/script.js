8e88b2ce4f8dbb0847ebbcd0ab65c020c3efb8fe
Daniel77apr <105032333+Daniel77apr@users.noreply.github.com>
2024-05-20 22:31:49 -0600

    Add Galería

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -16,3 +16,37 @@ window.onscroll = () => {
     
     })
 }
+
+
+const galleryImgs = document.getElementsByClassName("gallery-img");
+const slide = document.getElementById("slide");
+const back = document.getElementById("back");
+const forward = document.getElementById("forward");
+
+let imgCounter = 0;
+
+function nextImg(direction) {
+    return function() {
+        imgCounter += direction;
+        if(imgCounter < 0) {
+            imgCounter = galleryImgs.length - 1;
+        } else if(imgCounter >= galleryImgs.length) {
+            imgCounter = 0;
+        }
+        slide.src = galleryImgs[imgCounter].src;
+        console.log(imgCounter);
+    }
+}
+
+function selectImg(src) {
+    return function() {
+        imgCounter = parseInt(this.dataset.index);    
+        slide.src = src
+    }
+}
+
+for(let element of galleryImgs) {
+    element.addEventListener("click", selectImg(element.src));
+}
+back.addEventListener("click", nextImg(-1));
+forward.addEventListener("click", nextImg(1));
